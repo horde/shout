59fc6f06f14e298b1616753de45ac84840b424cc
Ralf Lang <ralf.lang@ralf-lang.de>
2026-05-19 19:47:20 +0200

    chore: Update to PHP 8

---
+++ src/Shout.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Copyright 2013-2026 The Horde Project (http://www.horde.org/)
+ *
+ * See the enclosed file LICENSE for license information. If you
+ * did not receive this file, see http://www.horde.org/licenses/.
+ *
+ * @author   Jan Schneider <jan@horde.org>
+ * @category Horde
+ * @package  Shout
+ */
+
+declare(strict_types=1);
+
+namespace Horde\Shout;
+
+/**
+ * Shout application class.
+ *
+ * @author    Jan Schneider <jan@horde.org>
+ * @category  Horde
+ * @copyright 2013-2026 The Horde Project
+ * @package   Shout
+ */
+class Shout
+{
+}
